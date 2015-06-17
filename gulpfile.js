f330ec554f15d74994657c1d353a3cbb66266448
Jim Friend <jfriend@guthy-renker.com>
2015-06-17 16:04:13 -0700

    Finished removing unused variables and refactoring all dir and paths to the paths and sources objects. Added some additional logging to the console output when running gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// @TODO
+// Add gutil logging with colors for better output in console
+// Make sure watch task is working
+
 var gulp 			= require( 'gulp' ),
 	gutil 			= require( 'gulp-util' ),
 	concat 			= require( 'gulp-concat' ),
@@ -13,84 +17,69 @@ var gulp 			= require( 'gulp' ),
 	plumber			= require( 'gulp-plumber' ),
 	jsonminify		= require( 'gulp-jsonminify' );
 
-var env,
-	jsSources,
-	sassSources,
-	htmlSources,
-	jsonSources,
-	outputDir,
-	config;
-
 var paths = {
-	config					: {
-		bower 					: './bower_components',
-		node 					: './node_modules'
+	config				: {
+		bower 			: './bower_components',
+		node 			: './node_modules'
 	},
-	dev 					: {
-		base 				: './builds/development',
-		css 				: './builds/development/css',
-		fonts 				: './builds/development/fonts',
-		images 				: './builds/development/images',
-		js 					: './builds/development/js'
+	dev 				: {
+		base 			: './builds/development',
+		images 			: './builds/development/images',
+		css 			: './builds/development/css',
+		js 				: './builds/development/js',
+		fonts 			: './builds/development/fonts'
 	},
 	pub					: {
-		base 				: './builds/production',
-		css 				: './builds/production/css',
-		fonts 				: './builds/production/fonts',
-		js 					: './builds/production/js'
+		base 			: './builds/production',
+		css 			: './builds/production/css',
+		js 				: './builds/production/js',
+		fonts 			: './builds/production/fonts'
 	},
-	components 				: {
-		base				: './components',
-		sass 				: './components/sass',
-		js 					: './components/js'
+	components 			: {
+		base			: './components',
+		sass 			: './components/sass',
+		js 				: './components/scripts'
 	},
-	vendor					: {
-		bootstrap 			: {
-			base			: './bower_components/bootstrap-sass',
-			fonts 			: './bower_components/bootstrap-sass/assets/fonts',
-			images 			: './bower_components/bootstrap-sass/assets/images',
-			js 				: './bower_components/bootstrap-sass/assets/javascripts',
-			css 			: './bower_components/bootstrap-sass/assets/stylesheets'
+	vendor				: {
+		bootstrap 		: {
+			base		: './bower_components/bootstrap-sass',
+			images 		: './bower_components/bootstrap-sass/assets/images',
+			css 		: './bower_components/bootstrap-sass/assets/stylesheets',
+			js 			: './bower_components/bootstrap-sass/assets/javascripts',
+			fonts 		: './bower_components/bootstrap-sass/assets/fonts'
 		},
-		fontawesome			: {
-			base            : './bower_components/fontawesome',
-			css 			: './bower_components/fontawesome/css',
-			fonts 			: './bower_components/fontawesome/fonts',
-			sass 			: './bower_components/fontawesome/scss'
+		fontawesome		: {
+			base        : './bower_components/fontawesome',
+			css 		: './bower_components/fontawesome/css',
+			sass 		: './bower_components/fontawesome/scss',
+			fonts 		: './bower_components/fontawesome/fonts'
 		}
 
 	}
 };
 
-config = {
-	bowerDir		: './bower_components',
-	bootstrapDir 	: './bower_components/bootstrap-sass',
-	nodeDir			: './node_modules'
+var sources = {
+	html 				: [paths.dev.base + '/*.html'],
+	css 				: [paths.components.sass + '/style.scss'],
+	js 					: [paths.components.js + '/*.js'],
+	json 				: [paths.dev.js + '/*.json'],
+	fonts 				: [paths.vendor.bootstrap.fonts + '/**.*'],
+	icons 				: [paths.vendor.fontawesome.fonts + '/**.*']
 };
 
-env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 
+var outputDir;
 if( env === 'development' ) {
 	outputDir = paths.dev.base;
 } else {
 	outputDir = paths.pub.base;
 }
 
-sassSources = ['components/sass/style.scss'];
-htmlSources = [outputDir + '/*.html'];
-jsonSources = [outputDir + '/js/*.json'];
-jsSources = ['components/scripts/*.js'];
-
-var onError = function( err ) {  
-	gutil.beep();
-	console.log( err );
-	this.emit( 'end' );
-};
-
 gulp.task( 'default', ['html', 'json', 'css', 'fonts', 'icons', 'js', 'connect', 'watch'] );
 
 gulp.task( 'watch', function() {
-	gulp.watch( jsSources, ['js'] );
+	gulp.watch( sources.js, ['js'] );
 	gulp.watch( 'components/sass/*.scss', ['css'] );
 	gulp.watch( 'builds/development/*.html', ['html'] );
 	gulp.watch( 'builds/development/*.json', ['json'] );
@@ -104,39 +93,17 @@ gulp.task( 'connect', function() {
 });
 
 gulp.task( 'html', function() {
-	gulp.src( paths.dev.base + '/*.html' )
+	gulp.src( sources.html )
 		.pipe( plumber({
       		errorHandler : onError
     	}))
 		.pipe( gulpif( env === 'production', minifyhtml() ) )
 		.pipe( gulpif( env === 'production', gulp.dest( outputDir ) ) )
-		.pipe( connect.reload() )
-});
-
-gulp.task( 'json', function() {
-	gulp.src( paths.dev.js + '*.json' )
-		.pipe( plumber({
-      		errorHandler : onError
-    	}))
-		.pipe( gulpif( env === 'production', jsonminify() ) )
-		.pipe( gulpif( env === 'production', gulp.dest( paths.pub.js ) ) )
-		.pipe( connect.reload() )
-});
-
-gulp.task( 'js', function() {
-	gulp.src( jsSources )
-		.pipe( plumber({
-      		errorHandler : onError
-    	}))
-		.pipe( concat( 'script.js' ) )
-		.pipe( browserify() )
-		.pipe( gulpif(env === 'production', uglify() ) )
-		.pipe( gulp.dest( outputDir + '/js' ) )
-		.pipe( connect.reload() )
+		.pipe( connect.reload() );
 });
 
 gulp.task( 'css', function() {
-	gulp.src( paths.components.sass + '/style.scss' )
+	gulp.src( sources.css )
 		.pipe( plumber({
       		errorHandler : onError
     	}))
@@ -148,23 +115,53 @@ gulp.task( 'css', function() {
 		.pipe( gulpif(env === 'production', minifycss() ) )
 		.pipe( sourcemaps.write() )
 		.pipe( gulp.dest( outputDir + '/css') )
-		.pipe( connect.reload() )
+		.pipe( connect.reload() );
+});
+
+gulp.task( 'js', function() {
+	gulp.src( sources.js )
+		.pipe( plumber({
+      		errorHandler : onError
+    	}))
+		.pipe( concat( 'script.js' ) )
+		.pipe( browserify() )
+		.pipe( gulpif(env === 'production', uglify() ) )
+		.pipe( gulp.dest( outputDir + '/js' ) )
+		.pipe( connect.reload() );
+	gutil.log( gutil.colors.green( 'From: ' +  sources.js ) );
+	gutil.log( gutil.colors.green( 'To: ' +  outputDir + '/js' ) );
+});
+
+gulp.task( 'json', function() {
+	gulp.src( sources.json )
+		.pipe( plumber({
+      		errorHandler : onError
+    	}))
+		.pipe( gulpif( env === 'production', jsonminify() ) )
+		.pipe( gulpif( env === 'production', gulp.dest( paths.pub.js ) ) )
+		.pipe( connect.reload() );
 });
 
 gulp.task( 'fonts', function() {
-    gulp.src( paths.vendor.bootstrap.fonts + '/**/*' )
+    gulp.src( sources.fonts )
 		.pipe( plumber({
       		errorHandler : onError
     	}))
     	.pipe( gulp.dest( outputDir + '/fonts') )
-    	.pipe( connect.reload() )
+    	.pipe( connect.reload() );
 });
 
 gulp.task( 'icons', function() { 
-    gulp.src( paths.vendor.fontawesome.fonts + '/**.*' ) 
+    gulp.src( sources.icons ) 
 		.pipe( plumber({
       		errorHandler : onError
     	}))
     	.pipe( gulp.dest( outputDir + '/fonts' ) )
-    	.pipe( connect.reload() )
+    	.pipe( connect.reload() );
 });
+
+var onError = function( err ) {  
+	gutil.beep();
+	console.log( err );
+	this.emit( 'end' );
+};
